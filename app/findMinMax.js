a1fef664f44c82811be2870e6bfd77ed049cef77
Akinbode Femi <femi661@gmail.com>
2017-01-07 07:24:28 +0100

    Fixed bugs and added comments

--- app/findMinMax.js
+++ app/findMinMax.js
@@ -1,32 +1,58 @@
 module.exports = {
  
- computeAverage: function(num1,num2,num3) {
-   var average = 0;
-   average = ((num1 + num2 + num3)/3);
-   return average;
- },
- 
- computeFactorial: function(num) {
-   var factorial = 1;
-   var i = 1;
-   while (i <= num)
-   {
-   	factorial = factorial * i;
-   	i = i + 1;
-   }
-   return factorial; 
- },
+ findMinMax: function(arr) {
+   var minMax = [];
+  // Check if the argument is an array
+   if (arr instanceof Array) 
+      {
+         //Initialize min and max to the first array element
+         var min = arr[0];
+         var max = arr[0];
+         //Initialize iterator to 0
+         var iterator = 0;
+         // Iteration of the array
+         while(iterator < arr.length)
+         {
+            //Check if the current element is a Number
+            if(typeof(arr[iterator]) == 'number' )
+            {
+               //Check if current element is less than min and assign current element to min if true
+               if(min > arr[iterator])
+               {
+                  min = arr[iterator];
+               }
+               //Check if the current element is greater than max and assign current element to max if true
+               if (max < arr[iterator])
+               {
+                  max = arr[iterator];
+               }
 
- convertTempCtoF: function(cTemp) {
-   var fTemp = 0;
-   fTemp = (cTemp * (9/5) + 32);
-   return fTemp; 
- },
-
- convertTempFtoC: function(fTemp) {
-   var cTemp = 0;
-   cTemp = (fTemp - 32)/(9/5);
-   return cTemp; 
+               iterator += 1;          
+            }
+            //Returns `invalid argument if current element is not a number
+            else
+            {
+               return 'invalid argument';
+            }            
+         }
+         //Checks if min equals max 
+         if(min === max)
+         {
+            minMax[0] = min;
+         }
+         //Execute if min does not equal max
+         else
+         {
+            minMax[0] = min;
+            minMax[1] = max;
+         }
+         return minMax;
+      }
+      //Returns `invalid argument if argument is not an array
+   else
+      {
+        return 'invalid argument';
+      }
  }
-
+  
 }
